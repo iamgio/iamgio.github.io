d6ebbbf95621468fd27fd4684a2754d093464a63
Giorgio Garofalo <giogar2705@gmail.com>
2025-12-04 22:46:28 -0800

    Add blog template

--- src/js/blog.js
+++ src/js/blog.js
@@ -2,10 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const blogItems = document.querySelectorAll('.blog-item');
     Array.from(blogItems).forEach(blogItem => {
         const linkElement = getBlogItemLinkElement(blogItem);
-        const blogDate = getBlogItemDate(linkElement);
-
-        const dateElement = createBlogItemDateElement(blogDate);
-        blogItem.insertBefore(dateElement, blogItem.firstChild);
 
         // Apply href to blog item and remove the link element
         const href = linkElement.getAttribute('href');
@@ -27,25 +23,4 @@ function getBlogItems() {
  */
 function getBlogItemLinkElement(blogItem) {
     return blogItem.querySelector('a:first-child');
-}
-
-/**
- * The first element of the blog item is an <a> tag, whose text content is: "YYYY-MM-DD-title".
- * This function extracts the date from that text content and returns it as a Date object.
- * @param {Element} blogItemLinkElement - The <a> element of the blog item.
- * @returns {Date} - The date of the blog item.
- */
-function getBlogItemDate(blogItemLinkElement) {
-    const dateString = blogItemLinkElement.textContent.split('-').slice(0, 3).join('-');
-    return new Date(dateString);
-}
-
-function createBlogItemDateElement(date) {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedDate = date.toLocaleDateString(undefined, options);
-
-    const dateElement = document.createElement('div');
-    dateElement.className = 'blog-item-date';
-    dateElement.textContent = formattedDate;
-    return dateElement;
 }
